c47ee460d360e27e73f9c8e2b59e2be50d346914
Dexctor <contactantoinedewas@gmail.com>
2024-02-14 15:51:32 +0100

    link

--- src/Loader.js
+++ src/Loader.js
@@ -1,10 +1,10 @@
-// Loader.js
+
 import React from 'react';
 
 const Loader = () => {
   return (
     <div className="loader-container">
-      {/* Vous pouvez utiliser une image ou un CSS pour créer l'effet de spinner */}
+     
       <div className="loader">Chargement...</div>
     </div>
   );
